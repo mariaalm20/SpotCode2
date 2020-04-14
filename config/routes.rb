c3de4d3e21e075fa5d2bec3912ceb6b6900934f4
mariaalm20 <maria20fernanda@gmail.com>
2020-04-14 15:35:30 -0300

    backend

---
+++ config/routes.rb
@@ -0,0 +1,28 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  devise_for :users
+  root to: "home#index"
+
+  concern :favoritable do |options|
+    shallow do
+      post "/favorite", { to: "favorites#create", on: :member }.merge(options)
+      delete "/favorite", { to: "favorites#destroy", on: :member }.merge(options)
+    end
+  end
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :dashboard, only: :index
+      resources :categories, only: %i[index show]
+      resources :search, only: :index
+      resources :albums, only: :show do
+       resources :recently_heards, only: :create
+    end 
+      resources :favorites, only: :index
+
+      resources :songs, only: [] do
+        concerns :favoritable, favoritable_type: 'Song'
+      end  
+  end
+  end
+ end
